14e63b843cd78c21c95c47c8324f60de5715d6db
Nithya <nithyasrisundaram@gmail.com>
2026-01-27 12:54:15 +0530

    Initial commit

---
+++ src/main/java/com/ecg/ECGApplication/EcgApplication.java
@@ -0,0 +1,13 @@
+package com.ecg.ECGApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecg/ECGApplication/EcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecg.ECGApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
